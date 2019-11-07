a80c308b1b47a258c3118890a854d337cd9dec34
Owain Lewis <owain.lewis@oracle.com>
2019-11-07 21:03:49 +0000

    Cleanup tree

--- src/main/java/com/owainlewis/dsa/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/main/java/com/owainlewis/dsa/datastructures/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package com.owainlewis.dsa.datastructures.binarysearchtree;
 
+import java.util.Comparator;
+
 /**
  * A binary search tree (BST) is a data structure which has the following properties:
  *
@@ -15,21 +17,46 @@ public final class BinarySearchTree<T extends Comparable<T>> {
 
     private BinaryTreeNode<T> root;
 
-    public void insert (T data) {
-        if (root == null) {
-            root = new BinaryTreeNode<>(data);
-            return;
-        }
+    private Comparator<T> comparator;
 
-        int cmp = root.getData().compareTo(data);
-
-        if (cmp == 0) {
-            return;
-        } else if (cmp < 0) {
-            return;
-        } else {
-            return;
-        }
+    public BinarySearchTree(Comparator<T> c) {
+        root = null;
+        comparator = c;
     }
 
+    private int compare(T x, T y) {
+        if (comparator == null) return x.compareTo(y);
+        return comparator.compare(x,y);
+    }
+
+    /**
+     * Insert a value into the tree
+     *
+     * @param value A value to insert
+     */
+    public void insert(T value) {
+        root = insertRecursive(root, value);
+    }
+
+    private BinaryTreeNode<T> insertRecursive(BinaryTreeNode<T> current, T value) {
+        if (current == null)
+            return new BinaryTreeNode<>(value);
+        int comparision = compare(value, current.getData());
+        if (comparision == 0)
+            return current;
+        if (comparision < 0)
+            current.setLeft(insertRecursive(current.getLeft(), value));
+        else
+            current.setRight(insertRecursive(current.getRight(), value));
+        return current;
+    }
+
+    public int height() {
+        return height(root);
+    }
+
+    private int height(BinaryTreeNode<T> tree) {
+        if (tree == null) return -1;
+        return 1 + Math.max(height(tree.getLeft()), height(tree.getRight()));
+    }
 }
