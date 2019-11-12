fb61e14cb34a70db43f5a4684d1b6053296aa04b
Owain Lewis <owain.lewis@oracle.com>
2019-11-12 17:49:21 +0000

    Update TernarySearchTree.java

--- src/main/java/com/owainlewis/dsa/datastructures/tree/TernarySearchTree.java
+++ src/main/java/com/owainlewis/dsa/datastructures/tree/TernarySearchTree.java
@@ -1,9 +1,9 @@
 package com.owainlewis.dsa.datastructures.tree;
 
 /**
- * A Ternary Search Tree is a special Trie data structure which resolves the memory consumption of the Trie data structure,
- * in this tree each node has only three children. Left and Right which are the same as in
- * Binary search tree. Mid child contains the next letter of the word which parent is part of.
+ * A Ternary Search Tree is a special Trie data structure which resolves the memory consumption of the Trie data structure.
+ * In this tree each node has only three children, left and right which are the same as in
+ * Binary Search Tree. The middle child contains the next letter of the word which the parent is part of.
  */
 public final class TernarySearchTree {
 }
